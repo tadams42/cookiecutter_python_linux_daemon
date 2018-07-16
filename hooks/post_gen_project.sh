3c7698ff370ecab9161ce509b2673313d7d3370a
Tomislav Adamic <tomislav@sedmiodjel.com>
2018-07-16 11:55:49 +0200

    fix: post-gen git script no longer tries to initialize already initialized git repo.

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-git init .
-git commit --allow-empty -m "Initial"
-git checkout -b development
+if [ ! -d ".git" ]; then
+    git init .
+    git commit --allow-empty -m "Initial"
+    git checkout -b development
 
-cd config
-ln -s ../src/{{cookiecutter.project_slug}}/resources/development.yaml
-ln -s ../src/{{cookiecutter.project_slug}}/resources/test.yaml
-ln -s ../src/{{cookiecutter.project_slug}}/resources/logging_config.json
-cd ..
+    cd config
+    ln -sf ../src/{{cookiecutter.project_slug}}/resources/development.yaml
+    ln -sf ../src/{{cookiecutter.project_slug}}/resources/test.yaml
+    ln -sf ../src/{{cookiecutter.project_slug}}/resources/logging_config.json
+    cd ..
 
-git add .
-git commit -m "Application scaffold"
-
+    git add .
+    git commit -m "Application scaffold"
+fi
